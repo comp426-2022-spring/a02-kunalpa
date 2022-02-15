1292b91b626fc64dadf0a06f46c1b929a472acda
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-02-15 16:20:44 -0500

    reverted back

--- modules/coin.mjs
+++ modules/coin.mjs
@@ -72,12 +72,12 @@ function countFlips(array) {
       tailNums ++
     }
   }
-  // if(headNums == 0){
-  //   return { tails: tailNums }
-  // }
-  // else if (tailNums == 0){
-  //   return { heads: headNums}
-  // }
+  if(headNums == 0){
+    return { tails: tailNums }
+  }
+  else if (tailNums == 0){
+    return { heads: headNums}
+  }
   return {heads: headNums, tails: tailNums }
 }
 
